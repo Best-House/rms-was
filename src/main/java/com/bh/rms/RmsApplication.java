df01d96fc4544b1ab60f56467eb2961872887937
hojun.sung <hjsung809@gmail.com>
2023-05-05 22:44:52 +0900

    Rename RmsApplication

--- src/main/java/com/bh/rms/RmsApplication.java
+++ src/main/java/com/bh/rms/RmsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceApplication {
+public class RmsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication.run(RmsApplication.class, args);
     }
 
 }
