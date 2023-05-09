e2c1d4cdcdba2226f3cb5a4d55ae346fc31b603b
hojun sung <43060547+hjsung809@users.noreply.github.com>
2023-05-09 23:54:56 +0900

    Change docker-compose (#11)
    * Fix scp, ssh
    
    ---------
    
    Co-authored-by: hojun.sung <hjsung809@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker-compose -f ~/rms-was/docker-compose.yml down
-docker-compose -f ~/rms-was/docker-compose.yml up -d
+/usr/local/bin/docker-compose -f ~/rms-was/docker-compose.yml down
+/usr/local/bin/docker-compose -f ~/rms-was/docker-compose.yml up -d
