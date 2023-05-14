d8a3114d6f623b283618859a2d598535f75e18df
hojun.sung <hjsung809@gmail.com>
2023-05-14 20:53:27 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 /usr/local/bin/docker-compose -f ~/rms-was/docker-compose.yml down
+/usr/local/bin/docker pull hjsung809/rms:latest
 /usr/local/bin/docker-compose -f ~/rms-was/docker-compose.yml up -d
