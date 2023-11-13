9a0106a7ca0e9176599901932a37d6e0f4f6830d
hojun.sung <hjsung809@gmail.com>
2023-11-13 16:22:40 +0900

    Add ascii doc(ing..)

--- src/test/java/com/bh/rms/controller/MaterialControllerTest.java
+++ src/test/java/com/bh/rms/controller/MaterialControllerTest.java
@@ -3,38 +3,62 @@ package com.bh.rms.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureRestDocs
 @SpringBootTest
-@ExtendWith({RestDocumentationExtension.class})
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
 class MaterialControllerTest {
+    @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
-
     @Test
-    public void test() throws Exception {
-        this.mockMvc.perform(get("/api").accept(MediaType.APPLICATION_JSON))
+    public void createMaterials() throws Exception {
+        this.mockMvc.perform(
+                        RestDocumentationRequestBuilders.post("/api/materials")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"name\": \"water\",\n" +
+                                "    \"price\": 2000,\n" +
+                                "    \"amount\": 1000\n" +
+                                "}")
+                )
                 .andExpect(status().isOk())
-                .andDo(document("index"));
+                .andDo(
+                        document("materials-create",
+                                requestFields(
+                                        fieldWithPath("name").description("name"),
+                                        fieldWithPath("price").description("price").optional(),
+                                        fieldWithPath("amount").description("amount").optional()
+                                ),
+                                responseFields( // response 필드 정보 입력
+                                        fieldWithPath("id").description("ID"),
+                                        fieldWithPath("name").description("name"),
+                                        fieldWithPath("priceInfo").description("optional").optional(),
+                                        fieldWithPath("priceInfo.price").description("price >= 0"),
+                                        fieldWithPath("priceInfo.amount").description("amount > 0")
+                                )
+                        )
+                );
 
     }
 }
