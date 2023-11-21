4b9a111af3a36bd07abe9f99a9dfd478aeac75d8
hojun.sung <hjsung809@gmail.com>
2023-11-21 21:28:32 +0900

    Make Ingredient VO

--- src/main/java/com/bh/rms/domain/aggregate/recipe/Recipe.java
+++ src/main/java/com/bh/rms/domain/aggregate/recipe/Recipe.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,7 +56,7 @@ public class Recipe { // root aggregate
                     throw new InvalidIngredientAmountException();
                 }
             }
-            this.ingredients = Collections.unmodifiableList(ingredients);
+            this.ingredients = List.copyOf(ingredients); // immutable list
         }
     }
 
