a45b914c6f7c3c8c5958aa1336423d2a41d49ca5
hyeseon.choi <hyeseon.choi@navercorp.com>
2023-12-02 00:14:17 +0900

    Add menu

---
+++ src/main/java/com/bh/rms/domain/aggregate/menu/Menu.java
@@ -0,0 +1,58 @@
+package com.bh.rms.domain.aggregate.menu;
+
+import com.bh.rms.domain.exception.InvalidAggregateIdException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
+
+@Getter
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Menu {
+    private String id;
+    private String name;
+    private Integer price;
+    private String recipeId;
+
+    public Menu(String id, String name, Integer price, String recipeId) {
+        setId(id);
+        setName(name);
+        setPrice(price);
+        setRecipeId(recipeId);
+    }
+
+    public Menu(String name, Integer price, String recipeId) {
+        setName(name);
+        setPrice(price);
+        setRecipeId(recipeId);
+    }
+
+    public void setId(String id) {
+        if (StringUtils.isBlank(id)) {
+            throw new InvalidAggregateIdException();
+        }
+        this.id = id;
+    }
+
+    public void setRecipeId(String recipeId) {
+        if (StringUtils.isBlank(recipeId)) {
+            throw new InvalidAggregateIdException();
+        }
+        this.recipeId = recipeId;
+    }
+
+    public void setPrice(Integer price) {
+        if (price == null || price < 0) {
+            throw new InvalidAggregateIdException();
+        }
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new InvalidAggregateIdException();
+        }
+        this.name = name;
+    }
+}
