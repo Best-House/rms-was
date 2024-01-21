66f06f13d87f58b25df7e8044d5b309d95724970
hyeseon.choi <hyeseon.choi@navercorp.com>
2024-01-21 20:18:31 +0900

    Add validation to the MenuUpateRequest

--- src/main/java/com/bh/rms/web/menu/MenuUpdateRequest.java
+++ src/main/java/com/bh/rms/web/menu/MenuUpdateRequest.java
@@ -1,10 +1,15 @@
 package com.bh.rms.web.menu;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MenuUpdateRequest {
+    @NotBlank
     private String name;
+    @Min(0)
     private Integer price;
+    @NotBlank
     private String recipeId;
 }
